b086bff6fc1449d2aca77e2633ef54a10bf4232f
Felipe <felipeagc2@gmail.com>
2019-03-09 02:51:17 -0300

    Make inline functions also static

--- fstd_bitset.h
+++ fstd_bitset.h
@@ -13,21 +13,21 @@ extern "C" {
     uint8_t bytes[(size + 7) / 8];                                             \
   }
 
-inline bool fstd_bitset_at(uint8_t *bitset, uint32_t pos) {
+static inline bool fstd_bitset_at(uint8_t *bitset, uint32_t pos) {
   uint32_t index = pos / 8;
   uint32_t bit = pos % 8;
 
   return (bitset[index] & (1UL << bit)) ? true : false;
 }
 
-inline void fstd_bitset_set(uint8_t *bitset, uint32_t pos, bool val) {
+static inline void fstd_bitset_set(uint8_t *bitset, uint32_t pos, bool val) {
   uint32_t index = pos / 8;
   uint32_t bit = pos % 8;
   bitset[index] |= (val ? 1UL : 0UL) << bit;
 }
 
 // Sets the bitset to zero
-inline void fstd_bitset_reset(uint8_t *bitset, uint32_t size) {
+static inline void fstd_bitset_reset(uint8_t *bitset, uint32_t size) {
   for (uint32_t i = 0; i < size / 8; i++) {
     bitset[i] = 0;
   }
